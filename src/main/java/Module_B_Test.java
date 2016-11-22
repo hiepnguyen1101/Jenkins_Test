b8bb987459204686ef1c0e52ac58e091699d3319
Hiệp Nguyễn Hoàng <Hip Nguyn Hong>
2016-11-22 20:50:48 +0700

    v1.01

--- src/main/java/Module_B_Test.java
+++ src/main/java/Module_B_Test.java
@@ -9,6 +9,6 @@ public class Module_B_Test {
     @Test
     public void test(){
         Module_B module_b=new Module_B();
-        assertEquals("This should return two",2,module_b.return_two());
+        assertEquals("This should return two",2,module_b.return_two()+1);
     }
 }
