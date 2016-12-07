4175d81b6f9aaecabe7c93e684db39073cb65690
Hiệp Nguyễn Hoàng <Hip Nguyn Hong>
2016-12-07 10:36:01 +0700

    v1.05

--- src/main/java/Module_A_Test.java
+++ src/main/java/Module_A_Test.java
@@ -8,11 +8,11 @@ public class Module_A_Test {
     @Test
     public void test(){
         Module_A module_a=new Module_A();
-        assertEquals("This should return 1",1,module_a.return_num()+1);
+        assertEquals("This should return 1",1,module_a.return_num());
     }
     @Test
     public void test2(){
         Module_A module_a=new Module_A();
-        assertEquals("This should return 2",2,module_a.return_num()+2);
+        assertEquals("This should return 2",2,module_a.return_num()+1);
     }
 }
